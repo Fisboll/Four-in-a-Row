5318dcc7606aa71218acb7b35422d6323620bbeb
Fisboll <fredrik96hansen@gmail.com>
2022-11-08 14:32:51 +0000

    JS

---
+++ assets/js/javascript.js
@@ -0,0 +1,36 @@
+function newGame(button) {
+    button.style.display = "none";
+    const game = document.getElementById('gameContainer')
+    //Columns
+    const Columns = [];
+    //array that stoes columns/rows
+    const slotsArray = [];
+    //determine the next color
+    let nextColor = "red";
+    //Create columns
+    for (let i=0; i<10;i++) {
+        const column = document.createElement("div");
+        dispatchEvent.className = "column";
+        game.appendChild(div);
+        Columns.push(column);
+    }
+    class Slot {
+        constructor(element, column, row) {
+            this.column = column;
+            this.row = row;
+            this.element = element;
+            this.state = "";
+        }
+        clicked() {}
+    }
+    //create slots and push to columns
+    Columns.forEach(el, col) => {
+        let slotColumn = [];
+        for(i=0;i<9;i++){
+            const div = document.createElement("div");
+            div.classList.add("slot");
+            
+        }
+    }
+
+}
