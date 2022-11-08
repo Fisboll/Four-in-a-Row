e0655a77e0fcb807ef906b4b84d09b697dc75728
Fisboll <fredrik96hansen@gmail.com>
2022-11-08 15:14:05 +0000

    fixed some miss typing in code.

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,18 +1,19 @@
 function newGame(button) {
     button.style.display = "none";
-    const game = document.getElementById('gameContainer')
+    const game = document.getElementById("gameContainer")
     //Columns
-    const Columns = [];
+    const columns = [];
     //array that stoes columns/rows
     const slotsArray = [];
     //determine the next color
     let nextColor = "red";
     //Create columns
-    for (let i=0; i < 10; i++) {
-        const column = document.createElement("div");
+    for (let i = 0; i < 10; i++) {
+        const column = document.createElement('div');
+        dispatchEvent.className = "column"
         column.className = "column";
         game.appendChild(column);
-        Columns.push(column);
+        columns.push(column);
     }
     class Slot {
         constructor(element, column, row) {
@@ -24,20 +25,21 @@ function newGame(button) {
         clicked() {}
     }
     //create slots and push to columns
-    Columns.forEach((el, col) => {
+    columns.forEach((el, col) => {
         let slotColumn = [];
         for(i = 0; i < 9; i++){
             const div = document.createElement("div");
             div.classList.add("slot");
+            el.appendChild(div);
             const slot = new Slot(div, col, i);
             slotColumn.push(slot);
             div.onclick = function() {
                 slot.clicked();
             };
         }
-        slotsArray.push(slotsColumn);
+        slotsArray.push(slotColumn);
     });
     console.log(slotsArray);
-
 }
 
+newGame(document.getElementById("playButton"));
