37d2ed5bd83d01b266954a2b4a0d8aa8df55fc18
Fisboll <fredrik96hansen@gmail.com>
2023-01-17 15:51:23 +0000

    Adding custom code to Javascript fixing finishing the wining and draw function.

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,188 +1,41 @@
 const game = document.getElementById("gameContainer");
 
-function newGame() {
-  const columns = [];
-  const slotsArray = [];
-  let nextColor = "red";
-  for (let i = 0; i < 7; i++) {
-    const column = document.createElement("div");
-    column.className = "column";
-    game.appendChild(column);
-    columns.push(column);
-  }
-  class Slot {
-    constructor(element, column, row) {
-      this.column = column;
-      this.row = row;
-      this.element = element;
-      this.state = "";
-    }
-    clicked() {
-      const el = this.element;
-      if (!el.classList.contains("clickable")) return;
-      el.style.backgroundColor = nextColor;
-      this.state = nextColor;
-      
-      //make this element not clickable
-      el.classList.remove("clickable");
-      //make next element clickable
-      if (slotsArray[this.column][this.row - 1]) {
-        slotsArray[this.column][this.row - 1].element.classList.add(
-          "clickable",
-          nextColor
-        );
-      }
-      //check for game over
-      if (isDraw(slotsArray) == true) gameOver(undefined);
+let nextColor = "red";
 
-      if (isWinner(this.column, this.row, nextColor, slotsArray) == true)
-        gameOver(nextColor);
+let columns = [];
+let slotsArray = [];
 
-      //change next color
-      let oldColor = nextColor;
-      nextColor == "red" ? nextColor = "yellow" : nextColor = "red";
-      document.querySelectorAll(".clickable").forEach((el) => {
-        el.classList.remove(oldColor);
-        el.classList.add(nextColor);
-      });
-    }
+class Slot {
+  constructor(element, column, row) {
+    this.column = column;
+    this.row = row;
+    this.element = element;
+    this.state = "";
   }
-  //create slots and push to columns
-  columns.forEach((el, col) => {
-    let slotColumn = [];
-    for (i = 0; i < 6; i++) {
-      const div = document.createElement("div");
-      div.classList.add("slot");
-      el.appendChild(div);
-      const slot = new Slot(div, col, i);
-      slotColumn.push(slot);
-      div.onclick = function () {
-        slot.clicked();
-      };
-      div.style.top = i * 100 + 2 + "px";
+  clicked() {
+    const el = this.element;
+    if (!el.classList.contains("clickable")) return;
+    el.style.backgroundColor = nextColor;
+    this.state = nextColor;
+
+    el.classList.remove("clickable");
+    if (slotsArray[this.column][this.row - 1]) {
+      slotsArray[this.column][this.row - 1].element.classList.add(
+        "clickable",
+        nextColor
+      );
     }
-    slotsArray.push(slotColumn);
-  });
-  slotsArray.forEach((col) => {
-    col[5].element.classList.add("clickable", nextColor);
-  });
-}
-//check if game is draw
-function isDraw(slotsArray) {
-  let isDraw = true;
-  slotsArray.forEach((col) => {
-    col.forEach((slot) => {
-      if (slot.state == "") isDraw = false;
+    
+    if (isDraw(slotsArray) == true) gameOver(undefined);
+
+    if (isWinner(this.column, this.row, nextColor, slotsArray) == true)
+      gameOver(nextColor);
+
+    let oldColor = nextColor;
+    nextColor == "red" ? nextColor = "yellow" : nextColor = "red";
+    document.querySelectorAll(".clickable").forEach((el) => {
+      el.classList.remove(oldColor);
+      el.classList.add(nextColor);
     });
-  });
-  return isDraw;
-}
-//test the lines to see if someone has won
-function testLines(lines, color, slotsArray) {
-  let connectedSlots = 1; //slots touching, if 4 you win
-  lines.forEach((line) => {
-    for (i = 0; i < line.length; i++) {
-      const slotLocation = line[i];
-      column = slotLocation[0];
-      row = slotLocation[1];
-      //don't allow for searching off screen
-      if (column >= 0 && column <= 6 && row >= 0 && row <= 5) {
-        //make sure it is defined
-        if (typeof slotsArray[column][row] !== "undefined") {
-          if (slotsArray[column][row].state == color) {
-            connectedSlots += 1;
-          } else break;
-        }
-      } else break;
-    }
-  });
-  if (connectedSlots >= 4) return true;
-  return false;
-}
-//check if there is a winner
-function isWinner(col, row, color, slotsArray) {
-  const winningLines = {
-    horizontal: [
-      [
-        [col - 1, row],
-        [col - 2, row],
-        [col - 3, row],
-      ],
-      [
-        [col + 1, row],
-        [col + 2, row],
-        [col + 3, row],
-      ],
-    ],
-    vertical: [
-      [
-        [col, row - 1],
-        [col, row - 2],
-        [col, row - 3],
-      ],
-      [
-        [col, row + 1],
-        [col, row + 2],
-        [col, row + 3],
-      ],
-    ],
-    diagonalLeft: [
-      [
-        [col - 1, row - 1],
-        [col - 2, row - 2],
-        [col - 3, row - 3],
-      ],
-      [
-        [col + 1, row + 1],
-        [col + 2, row + 2],
-        [col + 3, row + 3],
-      ],
-    ],
-    diagonalRight: [
-      [
-        [col - 1, row + 1],
-        [col - 2, row + 2],
-        [col - 3, row + 3],
-      ],
-      [
-        [col + 1, row - 1],
-        [col + 2, row - 2],
-        [col + 3, row - 3],
-      ],
-    ],
-  };
-  if (testLines(winningLines.horizontal, color, slotsArray) == true)
-    return true;
-
-  if (testLines(winningLines.vertical, color, slotsArray) == true) return true;
-  if (testLines(winningLines.diagonalLeft, color, slotsArray) == true)
-    return true;
-
-  if (testLines(winningLines.diagonalRight, color, slotsArray) == true)
-    return true;
-
-  return false;
-}
-//check if game is over
-function gameOver(winner) {
-  setScore(winner);
-  resetGame();
-  }
-//set score on the scoreboard
-function setScore(winner) {
-  if (winner !== undefined){
-    document.getElementById(winner + "Score").innerHTML =
-    parseInt(document.getElementById(winner + "Score").innerHTML) + 1;
   }
 }
-
-function resetGame() {
-   //delete game
-   document.querySelectorAll(".column").forEach((column) => {
-    column.innerHTML = "";
-    column.parentNode.removeChild(column);
-   });
-   newGame();
-  }
-
-  newGame();
